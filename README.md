99ba51d8393ae5d8959faf267f177d1541633b62
Scott Gall <scottarthurgall@gmail.com>
2021-02-25 14:01:11 -0600

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python-rps
-🗿, 📃, ✂️
+🗿, 📃, ✂️\
+[main.py]('main.py') includes code for a `'rock, paper, scissors'` game. When the script is run, the user is prompted in the terminal to enter 'r', 'p', or 's'. If a uses enters a valid letter, the game's results are printed to the terminal `(user won, user lost, tie)`.
---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+
+print("Let's play Rock Paper Scissors!")
+
+options = {"r": "rock", "p": "paper", "s": "scissors"}
+wins = {"r": "s", "p": "r", "s": "p"}
+
+computer_choice = random.choice(list(options.keys()))
+user_choice = input("make your choice: (r)ock, (p)aper, (s)cissors?")
+
+if user_choice not in list(options.keys()):
+  print("I don't understand that.")
+  print("Next time, choose 'r', 'p', or 's'.")
+elif user_choice == computer_choice:
+  print("You chose {}. The computer chose {}.".format(options[user_choice], options[computer_choice]))
+  print("Tie!")
+elif wins[user_choice] == computer_choice:
+  print("you chose {}. The computer chose {}".format(options[user_choice], options[computer_choice]))
+  print("Yay! You won!")
+else:
+  print("you chose {}. The computer chose {}".format(options[user_choice], options[computer_choice]))
+  print("Sorry. You lost.")
